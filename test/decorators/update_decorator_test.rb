21fbf0ba049c7a61fcfad9491676b24c0fa90363
Carol Nichols <carol.nichols@gmail.com>
2011-10-01 14:24:28 -0500

    sad, this is gonna need rails.

--- test/decorators/update_decorator_test.rb
+++ test/decorators/update_decorator_test.rb
@@ -1,9 +1,17 @@
-# Go watch destroyallsoftware videos and/or corey haines' fast tests talk
-# if you don't know why this is happeniing instead of requiring test_helper.
-
-require_relative '../../app/decorators/update_decorator'
-require "minitest/autorun"
+require_relative '../test_helper'
+require 'mocha'
 
 describe UpdateDecorator do
+  include TestHelper
+
+  describe "#reply" do
+    it "creates a reply link" do
+      mock_update = Update.new
+      mock_update.stubs(:id).returns("1234")
+
+      u = UpdateDecorator.decorate(mock_update)
+      assert_equal "<a class=\"reply\" href=\"?reply=1234\">reply</a>", u.reply
+    end
+  end
 
 end
